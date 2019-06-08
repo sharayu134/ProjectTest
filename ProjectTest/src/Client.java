2b270b4871cb0af69d033d2ff0883939994a0c4e
ysharayu18@gmail.com <ysharayu18@gmail.com>
2019-06-08 11:01:39 +0530

    Added classes Client and Server

---
+++ ProjectTest/src/Client.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Client 
+{
+ public static void main(String[] args) 
+  { 
+	 try(ServerSocket serverSocket=new ServerSocket(5001))
+	 {
+		 Socket socket=serverSocket.accept();
+		 System.out.println("Server Connected");
+		 BufferedReader input= new BufferedReader(
+				 new InputStreamReader(socket.getInputStream()));
+		 PrintWriter output= new PrintWriter(socket.getOutputStream(),true);
+		 while (true) 
+		 {
+			 String echoString=input.readLine();
+			 if( echoString.equals("exit"))
+			 {
+				 break;
+			 }
+			 output.println("server says"+echoString);
+		 }
+		 
+	 }
+	 catch(IOException e) {
+		 System.out.println(e.getMessage());
+	 }
+  }
+}
---
+++ ProjectTest/src/Server.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.*;
+public class Server
+{
+	public static void main(String[] args)
+	{
+		try(Socket socket = new Socket("localhost",5001) )
+		{
+			
+			
+			
+			BufferedReader inbuf= new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			PrintWriter opp= new PrintWriter(socket.getOutputStream(),true);
+			
+			Scanner scanner= new Scanner(System.in);
+			String echoMsg;
+			String response;
+			scanner.close();
+			do 
+			{
+				System.out.println("Enter msg for server");
+				echoMsg=scanner.nextLine();
+				opp.println(echoMsg);
+				
+				if(!echoMsg.equals("exit"))
+				{
+					response=inbuf.readLine();
+					System.out.println("Server Response :"+response);
+				}
+				
+				
+			}while(!echoMsg.equals("exit"));
+			
+			
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
